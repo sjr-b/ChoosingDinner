31f1226a8eb039c7848c25f8445799312cc4da19
Sophia Rodriguez-Bell <sophiarodriguezbell@students.berkeley.net>
2019-03-12 14:25:59 -0700

    keep going

--- src/Dish.java
+++ src/Dish.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Dish {
 
     String name;
@@ -5,6 +7,8 @@ public class Dish {
     int preparationTime;
     String [] tags;
     String seasonRequirements; // this is for holiday and season-appropriate food
+    static int dishQuantity = 0;
+    static ArrayList<Dish> dishes = new ArrayList<Dish>();
 
     public Dish(String name, String[] ingredients, int preparationTime, String[] tags, String seasonRequirements) {
         this.name = name;
@@ -12,6 +16,39 @@ public class Dish {
         this.preparationTime = preparationTime;
         this.tags = tags;
         this.seasonRequirements = seasonRequirements;
+        dishQuantity++;
+        dishes.add(this);
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public String[] getIngredients() {
+        return ingredients;
+    }
+
+    public int getPreparationTime() {
+        return preparationTime;
+    }
+
+    public String[] getTags() {
+        return tags;
+    }
+
+    public void setTags(String[] tags) {
+        this.tags = tags;
+    }
+
+    public String getSeasonRequirements() {
+        return seasonRequirements;
+    }
+
+    public static int getDishQuantity() {
+        return dishQuantity;
+    }
+
+    public static ArrayList<Dish> getDishes() {
+        return dishes;
+    }
 }
--- src/Runner.java
+++ src/Runner.java
@@ -1,9 +1,31 @@
 public class Runner {
 
-    Dish [] dishes;
-
     public static void main(String[] args) {
-        Dish latke = new Dish("latke", "");
+        // These are the default dishes.
+        String [] latkeIngredients = {"potato", "pepper", "butter"};
+        String [] latkeTags = {"favorite"};
+        Dish latke = new Dish("latke", latkeIngredients, 60, latkeTags, "none");
+        String [] stirFryIngredients = {"beef", "bell pepper", "onion", "water chesnut", "rice"};
+        String [] stirFryTags = {""};
+        Dish stirFry = new Dish("stir fry", stirFryIngredients, 45, stirFryTags, "none");
+        String [] albondigasIngredients = {"beef", "rice", "egg", "celery", "carrot", "potato"};
+        String [] albondigasTags = {""};
+        Dish albondigas = new Dish("albondigas", albondigasIngredients, 90, albondigasTags, "winter");
+        String [] porkAndSalsaIngredients = {"pork", "onion", "salsa", "green bean", "rice"};
+        String [] porkAndSalsaTags = {""};
+        Dish porkAndSalsa = new Dish("pork and salsa", porkAndSalsaIngredients,45, porkAndSalsaTags, "none");
+        String [] beefAndPotatoesIngredients = {"beef", "potato", "onion", "mushroom"};
+        String [] beefAndPotatoesTags = {""};
+        Dish beefAndPotatoes = new Dish("beef and potatoes", beefAndPotatoesIngredients, 45, beefAndPotatoesTags, "none");
+        String [] penneIngredients = {"chicken", "pesto", "penne"};
+        String [] penneTags = {"favorite"};
+        Dish penne = new Dish ("penne", penneIngredients, 30, penneTags, "none");
+        String [] picnicIngredients = {"baguette", "pirociutto", "cheese", "apple", "roast beef", "salami", "fruit salad", "pasta salad"};
+        String [] picnicTags = {"favorite"};
+        Dish picnic = new Dish ("picnic dinner", penneIngredients, 10, penneTags, "summer");
+        String [] tacosIngredients = {"chicken", "guacamole", "tortilla", "bean", "rice", "cilantro", "onion"};
+        String [] tacosTags = {"favorite"};
+        Dish tacos = new Dish ("chicken tacos", tacosIngredients, 20, tacosTags, "none");
     }
 
 }
